55d4c0ecfc6caa6954d696ab3d68b3c57219b51a
huynhlin <huynhlin@urbandaleschools.com>
2023-01-04 08:42:14 -0600

    mini done

--- 9.1_Mini_Function.py
+++ 9.1_Mini_Function.py
@@ -26,3 +26,43 @@ The function should return the value, not print the value.
 Also, while there is a min function built into Python, don't use it. 
 Please use if statements and practice creating it yourself.
 '''
+# first = input("Input a number.")
+# second = input("Input a number.")
+# third = input("Input a number.")
+# inputs = [first, second, third]
+
+
+def mini(first, second, third):
+    try:
+        if first < second and first < third:
+            return first
+        elif second < first and second < third:
+            return second
+        elif third < first and third < second:
+            return third
+        else:  # tie breakers
+            if first < second:
+                return first
+            if first < third:
+                return first
+            if second < third:
+                return second
+            if second < first:
+                return second
+            if third < first:
+                return third
+            if third < second:
+                return third
+
+    except TypeError:
+        inputs = [first, second, third]
+        inputs.sort()
+        return inputs[0]
+
+
+print(mini(7, 3, 5))
+print(mini(5, 5, 4))
+print(mini(2, 2, 3))
+print(mini(-2, -6, -100))
+print(mini("Z", "B", "A"))
+# it works :D
